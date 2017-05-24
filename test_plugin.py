3cef0e41d4b76a1a3e855734b6a035a6fe25e93f
ShallyBanh <banh.shally@gmail.com>
2017-05-23 22:38:01 -0600

    Can now handle boolean equality signs

--- test_plugin.py
+++ test_plugin.py
@@ -1,37 +1,41 @@
 import sublime, sublime_plugin  
 
-class StyleCode():
-    def __init__(self):
+class StyleCode( ):
+    def __init__( self ):
         self.brackets = 0
         self.new_file = ""
         self.line = ""
 
-    def ParseLine(self, line):
+    def ParseLine( self, line ):
         self.line = line
 
-    def StyleSpaces(self):
+    def StyleSpaces( self ):
         #Spaces for brackets ( )
-        begin = self.line.find('(')
-        if( begin!= -1 and self.line[begin +1: begin +2 ] != ' '):
+        begin = self.line.find( '(' )
+        if( begin != -1 and self.line[begin +1: begin +2 ] != ' ' ):
             self.line = self.line[:begin+1] + " " + self.line[begin+1:]
 
-        end = self.line.find(')')
-        if(end != -1 and self.line[end -1 : end ] != ' '):
+        end = self.line.find( ')')
+        if( end != -1 and self.line[end -1 : end ] != ' ' ):
             self.line = self.line[:end] + " " + self.line[end:]
 
-        #Spaces for equal
-        equal = self.line.find('=')
-        if(equal != -1 and self.line[equal +1 : equal +2] != ' '):
-            self.line = self.line[:equal+1] + " " + self.line[equal+1:]
-
-        lequal = self.line.find('=')
-        if(lequal != -1 and self.line[lequal - 1: lequal]!= ' '):
+        #Spaces for equal signs
+        equal = self.line.find( '=' )
+        while( True ):
+            if( self.line[equal +1 : equal +2] != ' ' and self.line[equal +1 : equal +2] != '=' ):
+                self.line = self.line[:equal+1] + " " + self.line[equal+1:]
+            equal = self.line.find( '=', equal + 1 );
+            if(equal == -1):
+                break
+            
+        lequal = self.line.find( '=' )
+        if( lequal != -1 and self.line[lequal - 1: lequal]!= ' ' ):
             self.line = self.line[:lequal] + " " + self.line[lequal:]
 
-    def StyleBrackets(self):
+    def StyleBrackets( self ):
         offset = 0
-        ifbrackets = self.line.find('{')
-        endbracket = self.line.find('}')
+        ifbrackets = self.line.find( '{' )
+        endbracket = self.line.find( '}' )
 
         if ifbrackets != -1:
             self.brackets += 1
@@ -43,26 +47,26 @@ class StyleCode():
         if self.brackets != 0:
             spaces = self.brackets + offset
             new_line = "\t" * spaces
-            new_line += self.line.lstrip()
+            new_line += self.line.lstrip( )
             self.new_file += new_line
             return
 
         self.new_file += self.line
 
-    def MakeItBetter(self, filename):
-        with open(filename, "r") as fp:
+    def MakeItBetter( self, filename ):
+        with open( filename, "r" ) as fp:
             for line in fp:
-                self.ParseLine(line)
+                self.ParseLine( line )
                 self.StyleSpaces( )
                 self.StyleBrackets( )             
 
-        file = open(filename, "w")
-        file.seek(0)
-        file.write(self.new_file)
+        file = open( filename, "w" )
+        file.seek( 0 )
+        file.write( self.new_file )
 
-class EventDump(sublime_plugin.EventListener):  
+class EventDump( sublime_plugin.EventListener ):  
 
-    def on_post_save(self, view): 
-        style = StyleCode()
-        style.MakeItBetter(view.file_name())
+    def on_post_save( self, view ): 
+        style = StyleCode( )
+        style.MakeItBetter( view.file_name( ))
         
